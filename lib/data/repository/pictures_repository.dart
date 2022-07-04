05c7333ab24a64d94a74246b0cd17b5824655706
OraneOrveillon <orane.orveillon@outlook.fr>
2022-07-04 17:45:56 +0200

    Fix for Halley's comet

--- lib/data/repository/pictures_repository.dart
+++ lib/data/repository/pictures_repository.dart
@@ -11,12 +11,14 @@ class PicturesRepository {
   /// '&media_type=image' to only get images and not videos etc...
   Future<Pictures> getPictures() async {
     final response = await http.get(
-      Uri.parse(url ?? 'https://images-api.nasa.gov/search?keywords=$bodyName&media_type=image&page=1'),
+      Uri.parse(
+        url ?? 'https://images-api.nasa.gov/search?keywords=${bodyName!.replaceAll("'", '')}&media_type=image&page=1',
+      ),
     );
     if (response.statusCode == 200) {
       return picturesFromMap(response.body);
     } else {
-      throw Exception("Failed to load pictures");
+      throw Exception('Failed to load pictures');
     }
   }
 }
